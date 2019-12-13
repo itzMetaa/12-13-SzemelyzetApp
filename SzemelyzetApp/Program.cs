65990421c85de2ea82a85ad04e389ecff9134e1e
Diak <asd@asd.asd>
2019-12-13 10:14:46 +0100

    init szemely

--- SzemelyzetApp/Program.cs
+++ SzemelyzetApp/Program.cs
@@ -10,6 +10,8 @@ namespace SzemelyzetApp
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
---
+++ SzemelyzetApp/Szemely.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzemelyzetApp
+{
+    class Szemely
+    {
+        string nev;
+        string beosztas;
+        DateTime szuletes;
+
+        ISet<Szemely> beosztottak = new SortedSet<Szemely>();
+
+        public Szemely(string nev, string beosztas, DateTime szuletes)
+        {
+            this.nev = nev;
+            this.beosztas = beosztas;
+            this.szuletes = szuletes;
+        }
+
+
+    }
+}
